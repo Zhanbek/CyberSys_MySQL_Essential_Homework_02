7abcac53a69d7343e36b0e394b52dbb03ee251a3
Sarsenov <zhanbek.sarsenov@gmail.com>
2026-04-12 18:20:49 +0500

    Додав Foreign Key

--- Task_02/cr_t_RegularClients.sql
+++ Task_02/cr_t_RegularClients.sql
@@ -4,7 +4,9 @@ create table RegularClients(
     RegisterDate date,
     Address varchar(255),
     TaxCode char(12),
+    EmpoyeeForContact int,
     Status Varchar(50),
     primary key(Id),
-    Unique(TaxCode)
+    Unique(TaxCode),
+    Foreign Key( EmpoyeeForContact) references Personal(Id)
 );
